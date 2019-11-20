83d16e273983413cd20144f0712a6b8c7c9c5b36
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-11-20 21:25:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,17 @@
+<<<<<<< HEAD
   require 'pry'
   class Artist
   attr_accessor :name
+=======
+  class Artist
+  attr_accessor :name, :songs
+>>>>>>> 628c7544a9ed5c2b6b93c19d538f218bfcdcef6b
   
   @@songcounter = 0
   
   def initialize(name)
   @name=name
+<<<<<<< HEAD
   
   end 
  
@@ -21,12 +27,21 @@ return @song_array
  end
 
   def add_song(song)
+=======
+  @songs = []
+  end 
+ 
+
+  def add_song(song)
+    @songs << song 
+>>>>>>> 628c7544a9ed5c2b6b93c19d538f218bfcdcef6b
     song.artist = self
     @@songcounter +=1 
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
+<<<<<<< HEAD
     song.artist = self
     @@songcounter +=1
   end
@@ -37,6 +52,21 @@ return @song_array
   end
 
   #binding.pry 
+=======
+    @songs << song
+    song.artist = self
+    @@songcounter +=1
+  end
+  
+ def songs 
+   @songs
+ end
+ 
+  def self.song_count
+    @@songcounter
+  end
+
+>>>>>>> 628c7544a9ed5c2b6b93c19d538f218bfcdcef6b
   
   
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,17 @@
 class Song 
   attr_accessor :name, :artist
+<<<<<<< HEAD
     @@all = []
+=======
+>>>>>>> 628c7544a9ed5c2b6b93c19d538f218bfcdcef6b
   
   def initialize(name, artist = nil)
     @name = name
     @artist = artist
+<<<<<<< HEAD
+=======
+    @@all = []
+>>>>>>> 628c7544a9ed5c2b6b93c19d538f218bfcdcef6b
     @@all << self
   end
   
